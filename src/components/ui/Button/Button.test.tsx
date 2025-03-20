46875daae25810ca79732e9356f42bdc0c10b5e9
sarathdev <sarathrajkrla@gmail.com>
2025-03-20 14:52:23 +0530

    feat : percommit hook

--- src/components/ui/Button/Button.test.tsx
+++ src/components/ui/Button/Button.test.tsx
@@ -3,16 +3,16 @@ import '@testing-library/jest-dom';
 import Button from './button';
 
 test('renders button with text', () => {
-    render(<Button>Click Me</Button>);
-    expect(screen.getByText('Click Me')).toBeInTheDocument();
+  render(<Button>Click Me</Button>);
+  expect(screen.getByText('Click Me')).toBeInTheDocument();
 });
 
 test('calls onClick when button is clicked', () => {
-    const handleClick = jest.fn();
-    render(<Button onClick={handleClick}>Click Me</Button>);
+  const handleClick = jest.fn();
+  render(<Button onClick={handleClick}>Click Me</Button>);
 
-    const button = screen.getByText('Click Me');
-    fireEvent.click(button);
+  const button = screen.getByText('Click Me');
+  fireEvent.click(button);
 
-    expect(handleClick).toHaveBeenCalledTimes(1);
+  expect(handleClick).toHaveBeenCalledTimes(1);
 });

825c735a6082778ce4ba08342242302ae4821a4a
sarathdev <sarathrajkrla@gmail.com>
2025-03-20 15:39:31 +0530

    feat: stable

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,20 @@
 import nextJest from 'next/jest';
 
 const createJestConfig = nextJest({
-    dir: './'
+  dir: './',
 });
 
 const customJestConfig = {
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-    testEnvironment: 'jsdom',
-    transform: {
-        '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest'
-    },
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1'
-    }
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/src/tests/e2e/'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
+  },
+
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 };
 
 export default createJestConfig(customJestConfig);
